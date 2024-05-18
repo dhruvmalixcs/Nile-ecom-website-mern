c188197d1b863bb20d3dfd1081c2e213781e7e4d
Siddharth <siddharth1360.be21@chitkara.edu.in>
2024-05-18 13:37:33 +0530

    added Cart folder

--- frontend/src/State/Product/Action.js
+++ frontend/src/State/Product/Action.js
@@ -1,3 +1,28 @@
-export const findProducts=(reqData)=>async(dispatch)=>{
+import { FIND_PRODUCTS_REQUEST, FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS, FIND_PRODUCT_FAILURE, FIND_PRODUCT_SUCCESS } from "./ActionType";
 
-}
+export const findProducts=(reqData)=>async(dispatch)=>{
+    dispatch({typr:FIND_PRODUCTS_REQUEST})
+    const {colors,sizes,minPrice,maxPrice,minDiscount,category,stock,sort,pageNumber,pageSize} = reqData;
+    try {
+        const {data} = api.get(`/api/products/color=${colors}&size=${sizes}&minPrice=${minPrice}&maxPrice=${maxPrice}
+        &minDiscount=${minDiscount}&category=${category}&stock=${stock}&sort=${sort}
+        &pageNumber=${pageNumber}&pageSize=${pageSize}`)
+
+        dispatch({type:FIND_PRODUCT_SUCCESS,payload:data})
+    } catch (error) {
+        dispatch({type:FIND_PRODUCT_FAILURE,payload:error.message})
+    }
+};
+
+
+export const findProductsById=(reqData)=>async(dispatch)=>{
+    dispatch({typr:FIND_PRODUCT_BY_ID_REQUEST})
+    const {productId} = reqData;
+    try {
+        const {data} = api.get(`/api/products/id/${productId}`)
+
+        dispatch({type:FIND_PRODUCT_BY_ID_SUCCESS,payload:data})
+    } catch (error) {
+        dispatch({type:FIND_PRODUCT_BY_ID_FAILURE,payload:error.message})
+    }
+};
